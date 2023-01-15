12558d8d77cd670d7dbbddd971bab2ade9e225a5
Md. Mazharul Islam <mazharulislam611@gmail.com>
2023-01-15 19:38:04 +0600

    done

---
+++ app.js
@@ -0,0 +1,21 @@
+const fileInput = document.querySelector("input"),
+downloadBtn = document.querySelector("button");
+
+downloadBtn.addEventListener("click", e =>{
+    e.preventDefault();
+    fetchFile(fileInput.value);
+});
+
+
+function fetchFile(url){
+    fetch(url).then(res => res.blob()).then(file =>{
+        let temUrl = URL.createObjectURL(file);
+        let aTag = document.createElement("a");
+        aTag.href = temUrl;
+        aTag.download = url.replace(/\/[^\/]*$/, '/175');
+        document.body.appendChild(aTag); //adding <a> tag inside body
+        aTag.click(); // clicking <a> Tag so the file download
+        aTag.remove(); // removing <a></a> Tag once file downloaded
+    })
+}
+
